bc7ef6e0933c54cb3e01774035ac030bdf4d231d
Nick J Lyon <nick.lyon@Nicks-MacBook-Pro.local>
2021-04-23 14:53:59 -0400

    Created the header for the tidy code

---
+++ Scripts/asclepias-tidy.R
@@ -0,0 +1,35 @@
+## ---------------------------------------------------------------------------------------- ##
+                        # Moranz et al. Butterfly Milkweed Project
+## ---------------------------------------------------------------------------------------- ##
+# Code written by Nicholas J Lyon
+
+# PURPOSE ####
+  ## This code tidys the raw data into a format that is ready for analysis and figure construction
+
+# Clear the environment
+rm(list = ls())
+
+# Set the working directory
+setwd("~/Documents/_Publications/2021_Moranz_Asclepias/Moranz-AsclepiasCollab")
+  ## Session -> Set Working Directory -> Choose Directory...
+
+# Call any needed libraries here (good to centralize this step)
+library(readxl); library(tidyverse); library(stringr)
+
+## ------------------------------------------------ ##
+                # Data Joining ####
+## ------------------------------------------------ ##
+mkwd.12 <- read_excel("./Data/Asclepias-2012-RAW.xlsx", sheet = )
+
+
+
+
+
+## ------------------------------------------------ ##
+                  # Data Tidying ####
+## ------------------------------------------------ ##
+
+
+
+# END ####
+
